90574857e372e667bead744078d9eed78c4efbc0
Boderio <boderiotrigan@hotmail.com>
2023-11-29 12:11:21 +0100

    cours 4 - Chatbox - gestion message

--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
 class Formulaire extends Component {
-    state = {  } 
+    state = { 
+        message : ''
+     } 
+
+     createMessage = () => {
+        const {pseudo, addMessage} = this.props
+
+        const message = {
+            pseudo: pseudo,
+            message: this.state.message
+        }
+
+        addMessage(message)
+
+        // reset
+        this.setState({message: ''})
+     }
+
+     handleChange = event => {
+        const message = event.target.value
+        this.setState({message:message})
+     }
+
+     handleSubmit = event => {
+        event.preventDefault()
+        this.createMessage()
+     }
+
     render() { 
         return (
-            <form className="form">
-                <textarea 
+            <form className="form" onSubmit={this.handleSubmit}>
+                <textarea
+                    value={this.state.message}
+                    onChange={this.handleChange} 
                     required
                     maxLength='140' 
                 />
